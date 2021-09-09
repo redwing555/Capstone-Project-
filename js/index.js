bac8728ad8e9f8986b25ab1302adbe8a612a93e4
Hichami Houssam <hichami.houssam@gmail.com>
2021-09-09 11:40:33 +0200

    las set of changes to margin and padding to body

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 const speakerContain = document.querySelector('.speakers-container');
 
-
 const speakersArray = [
   {
     name: 'Stephen Hawking',
@@ -40,11 +39,9 @@ const speakersArray = [
   },
 ];
 
-let speakers = `` ;
-
+let speakers = '';
 
 speakersArray.forEach((speaker) => {
-
   speakers += `<article class="speaker" >
                     
                             
@@ -63,72 +60,37 @@ speakersArray.forEach((speaker) => {
   </div>
   
 
-</article>`
+</article>`;
 
-speakerContain.innerHTML = speakers;
-
-
-
+  speakerContain.innerHTML = speakers;
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.nav-desktop-container');
 const body = document.querySelector('body');
 const close = document.querySelector('.close');
 
-
-
-
-
 function openMenu() {
+  menu.classList.add('clickme');
+  menu.classList.remove('nav-desktop-container');
+  hamburger.classList.remove('hamburger');
+  close.classList.add('close-active');
+  body.style.overflow = 'hidden';
+}
 
-    
+hamburger.addEventListener('click', openMenu);
 
-    menu.classList.add('clickme');
-    menu.classList.remove('nav-desktop-container');
-    hamburger.classList.remove('hamburger');
-    close.classList.add('close-active');
-    body.style.overflow = 'hidden';
-  }
+function closeMenu() {
+  menu.classList.remove('clickme');
+  hamburger.classList.add('hamburger');
+  close.classList.remove('close-active');
+  menu.classList.add('nav-desktop-container');
+  body.style.overflow = 'scroll';
+}
 
-  hamburger.addEventListener('click', openMenu);
-  
+close.addEventListener('click', closeMenu);
 
-  
-   function closeMenu() {
-
-    menu.classList.remove('clickme');
-    hamburger.classList.add('hamburger');
-    close.classList.remove('close-active');
-    menu.classList.add('nav-desktop-container');
-    body.style.overflow = 'scroll';
-
-
-    
-   }
-  
-   close.addEventListener('click', closeMenu);
-  
-   const navList = document.querySelectorAll('.mnitem');
-   for (let i = 0; i < navList.length; i += 1) {
-     navList[i].addEventListener('click', closeMenu);
-   }
+const navList = document.querySelectorAll('.mnitem');
+for (let i = 0; i < navList.length; i += 1) {
+  navList[i].addEventListener('click', closeMenu);
+}
